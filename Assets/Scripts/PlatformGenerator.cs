31bbd7f688a1e1d14ecb0cfac03e20579c389e43
Jxtrm <j.ecijar@gmail.com>
2023-08-27 00:25:18 +0200

    Platforms Generator
    Generador aleatorio de plataformas

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    public static PlatformGenerator instance;
+    [SerializeField] private GameObject originalPlatform;
+    [SerializeField] private Transform currentPlatform;
+    [SerializeField] private int platformsNumber;
+    private int direction;
+    
+    private void OnEnable()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    
+    void Start()
+    {
+        PlatformsGenerator();
+    }
+    
+    void PlatformsGenerator()
+    {
+        for(int i = 0; i<platformsNumber; i++)
+        {
+            direction = Random.Range(0, 2);
+            if (direction == 1)
+            {
+                currentPlatform = Instantiate(originalPlatform, currentPlatform.position + Vector3.right * 2, Quaternion.identity).transform;
+            }
+            else
+            {
+                currentPlatform = Instantiate(originalPlatform, currentPlatform.position + Vector3.forward * 2, Quaternion.identity).transform;
+            }
+        }
+    }
+}
